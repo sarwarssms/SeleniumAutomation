9e338c7dde6017db191782920531d17c9e7c6bb3
sarwarssm <murshed.shah@gmail.com>
2022-06-03 17:23:20 -0400

    Add files via upload

---
+++ src/test/java/PageObjects/HomePage.java
@@ -0,0 +1,30 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage {
+	public WebDriver driver;
+
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		// PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(id="searchInput")
+	private WebElement searchBox;
+
+	@FindBy(css = "#search-form > fieldset > button > i")
+	private WebElement wikiSearchButton;
+
+	public void clickwikiSearchButton() {
+		this.wikiSearchButton.click();
+	}
+
+	public void fillUpSearchBox(String searchString) {
+		this.searchBox.clear();
+		this.searchBox.sendKeys(searchString);
+	}
+
+}
---
+++ src/test/java/PageObjects/SearchResultPage.java
@@ -0,0 +1,38 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SearchResultPage {
+	public WebDriver driver;
+
+	public SearchResultPage(WebDriver driver) {
+		this.driver = driver;
+		// PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(css = "#searchInput")
+	private WebElement searchBox;
+
+	@FindBy(name = "go")
+	private WebElement searchButton;
+
+	@FindBy(css = "#firstHeading ")
+	private WebElement firstHead;
+
+	public void clickSearchButton() {
+		this.searchButton.click();
+	}
+
+	public void fillUpSearchBox(String searchString) {
+		this.searchBox.clear();
+		this.searchBox.sendKeys(searchString);
+	}
+
+	public String firstHeading() {
+		String heading = firstHead.getText().toString();
+		return heading;
+	}
+	
+}
